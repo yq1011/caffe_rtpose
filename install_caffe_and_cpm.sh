bee9db4ac2b0765a8b48ad43e893fef1a1b15835
yq1011 <liuxhy237@163.com>
2017-02-23 20:42:49 +0800

    Update install_caffe_and_cpm.sh

--- install_caffe_and_cpm.sh
+++ install_caffe_and_cpm.sh
@@ -96,13 +96,6 @@ if [ ! -f $coco_model ]; then
     wget http://posefs1.perception.cs.cmu.edu/Users/tsimon/Projects/coco/data/models/coco/pose_iter_440000.caffemodel -P $coco_folder
 fi
 exitIfError
-# MPI
-mpi_folder="$models_folder"mpi/""
-mpi_model="$mpi_folder"pose_iter_160000.caffemodel""
-if [ ! -f $mpi_model ]; then
-    wget http://posefs1.perception.cs.cmu.edu/Users/tsimon/Projects/coco/data/models/mpi/pose_iter_160000.caffemodel -P $mpi_folder
-fi
-exitIfError
 echo "Models downloaded"
 echo "------------------------- CPM Models Downloaded -------------------------"
 echo ""
